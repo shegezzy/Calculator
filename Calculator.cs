b78dd463d5578387fca619da55966031c07dac64
shegezzy <39660743+shegezzy@users.noreply.github.com>
2022-08-11 08:45:50 +0100

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public abstract class PackageRate
+    {
+
+        public float RailRate { get; private set; } = 0.5f;
+        public float AirRate { get; private set; } = 0.6f;
+        public float SeaRate { get; private set; } = 0.7f;
+        public abstract string CalculateShippingCost(float orderAmount, float weight);
+    }
+
+    public class PackageByRail : PackageRate
+    {
+        public override string CalculateShippingCost(float orderAmount, float weight)
+        {
+
+            return "The shipping cost for rail package is " + (orderAmount + (weight * RailRate));
+        }
+    }
+    public class PackageBySea : PackageRate
+    {
+        public override string CalculateShippingCost(float orderAmount, float weight)
+        {
+            return "The shipping cost for sea package is " + (orderAmount + (weight * SeaRate));
+        }
+    }
+
+    public class PackageByAir : PackageRate
+    {
+        public override string CalculateShippingCost(float orderAmount, float weight)
+        {
+            return "The shipping cost for air package is " + (orderAmount + (weight * AirRate));
+        }
+    }
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float airOrdAmt = 700f;
+            float seaOrdAmt = 400f;
+            float railOrdAmt = 150f;
+            Console.WriteLine("Welcome to Package Calc \n");
+            Console.WriteLine("What is the weight of your package in Kg");
+
+
+            try
+            {
+                string weight = Console.ReadLine();
+                float fltWeight;
+                bool isParsed = float.TryParse(weight, out fltWeight);
+                if (isParsed)
+                {
+                    var train = new PackageByRail();
+                    var ship = new PackageBySea();
+                    var air = new PackageByAir();
+
+                    Console.WriteLine(train.CalculateShippingCost(railOrdAmt, fltWeight));
+                    Console.WriteLine(ship.CalculateShippingCost(seaOrdAmt, fltWeight));
+                    Console.WriteLine(air.CalculateShippingCost(airOrdAmt, fltWeight));
+
+                }
+                else
+                { 
+                    Console.WriteLine("Invalid input");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error detected ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
